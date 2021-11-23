127b9402a7f05079e99032fca9b5939ba8727ffd
Xeros <xeros.log@gmail.com>
2021-11-23 14:55:54 +0900

    Slider 완벽 구현
    - Slider 기능 완벽하게 구현됨
    - 키워드 표 윤곽 구현 완료

--- Brunch.co.kr/assets/js/slider.js
+++ Brunch.co.kr/assets/js/slider.js
@@ -18,6 +18,9 @@ prev.style.visibility = "hidden";
 //전체 슬라이드 컨테이너 넓이 설정
 slides.style.width = "10000px";
 
+// 슬라이드 하단의 page-no 클래스를 가진 객체의 자식 요소 가져옴
+const slideNo = document.querySelectorAll(".slide-no-page");
+
 function moveSlide(num) {
     slides.style.left = -num * 1000 + 'px';
     currentIdx = num;
@@ -27,8 +30,11 @@ prev.addEventListener('click', function () {
     // currentIdx가 0이 아닐 경우에만 이전 페이지 이동 기능 활성화
     if (currentIdx !== 0) {
         moveSlide(currentIdx - 1);
-        console.log(currentIdx);
 
+        // pageNo 변경
+        slideNo[currentIdx].innerHTML = '<u class="active-page">' + 0 + (Number(currentIdx) + 1) + '</u></span>'
+        slideNo[currentIdx + 1].innerHTML = '0' + String(currentIdx + 2)
+        
         // 다음 슬라이드 버튼 활성화
         next.style.visibility = 'visible';
     }
@@ -41,7 +47,10 @@ next.addEventListener('click', function () {
     // currentIdx가 마지막 슬라이드일 경우 다음 버튼 작동 안함
     if (currentIdx !== maxCount - 1) {
         moveSlide(currentIdx + 1);
-        console.log(currentIdx);
+
+        // pageNo 변경
+        slideNo[currentIdx].innerHTML = '<u class="active-page">' + 0 + (Number(currentIdx) + 1) + '</u></span>'
+        slideNo[currentIdx - 1].innerHTML = '0' + String(currentIdx)
     
         // 이전 슬라이드 버튼 활성화
         prev.style.visibility = "visible";
@@ -52,3 +61,16 @@ next.addEventListener('click', function () {
         next.style.visibility = "hidden";
     }
 });
+
+// 페이지 넘버 클릭 이벤트 처리
+for (let i of slideNo) {
+    i.addEventListener("click", e => {
+        // 버튼 클릭 이벤트 발생 이전 pageNo에 강조 효과 제거
+        slideNo[currentIdx].innerHTML = '0' + String(currentIdx + 1)
+
+        moveSlide(Number(i.innerHTML) - 1)
+
+        // 버튼 클릭 이벤트 발생 이후 pageNo에 강조 효과 부여
+        slideNo[currentIdx].innerHTML = '<u class="active-page">' + 0 + (Number(currentIdx) + 1) + '</u></span>'
+    })
+}
