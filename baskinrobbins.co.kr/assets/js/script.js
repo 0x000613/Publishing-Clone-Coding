39bee301b5e3b68e5a280d0bd79ac5413c659afa
yang <ysw7939@gmail.com>
2021-11-25 23:03:50 +0900

    베라 상단 메뉴바 1차 수정

--- baskinrobbins.co.kr/assets/js/script.js
+++ baskinrobbins.co.kr/assets/js/script.js
@@ -82,3 +82,17 @@ for (i = 0; i < dts.length; i++) {
 events[eventIndex-1].style.display = "block";  
 dts[eventIndex-1].className += " active";
 }
+
+// 메뉴슬라이더 menu-bar-category
+const menu = document.querySelector("#menu-bar-category");
+const dropdownMenu = document.querySelector("#dropdown-wrapper");
+
+// 메뉴바에 mouseover 이벤트 발생시 드롭다운 메뉴 출력
+menu.addEventListener("mouseover", e => {
+    dropdownMenu.style.height = "274px";
+});
+
+// 드롭다운 메뉴에 mouseout 이벤트 발생시 드롭다운 메뉴 비활성화
+dropdownMenu.addEventListener("mouseout", e => {
+    dropdownMenu.style.height = "0px";
+});
