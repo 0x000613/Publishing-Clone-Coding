cec590d8c30e4666fcb2882dab930130c661039f
Xeros <xeros.log@gmail.com>
2021-11-26 18:21:21 +0900

    드롭다운 이미지 크래시 해결
    - 특수문자 오타 제거

--- starbucks.co.kr/assets/js/script.js
+++ starbucks.co.kr/assets/js/script.js
@@ -1,6 +1,11 @@
+// 헤더 메뉴
 const menus = document.querySelectorAll(".menu-bar-category");
+// 드롭다운 전체 영역 div
 const dropdownWrapper = document.querySelector("#dropdown-wrapper");
+// dropdownWrapper의 자식으로 들어가는 메뉴들
 const dropdownInner = document.querySelectorAll(".dropdown-wrapper-inner");
+// dropdownWrapper 하단 배경 이미지
+const dropdownUnderBG = document.querySelector(".dropdown-under-bg")
 
 // 메뉴바에 mouseover 이벤트 발생시 드롭다운 메뉴 출력
 for (let i = 0; i < menus.length; i++) {
@@ -11,13 +16,20 @@ for (let i = 0; i < menus.length; i++) {
         dropdownInner[i].style.display = "grid";
         // 드롭다운 메뉴 높이를 내부 div의 길이를 자동으로 인식하여 자동조정함
         // 아래 변수를 조정하여 자동조정 이외에 수동으로 높이 추가 역시 가능함
-        const customPlusHeight = 200;
+        const customPlusHeight = 0;
         dropdownWrapper.style.height = String(dropdownInner[i].scrollHeight + customPlusHeight) + "px";
+
+        // 드롭다운 하단 배경 이미지 활성화
+        dropdownUnderBG.style.display = "inline-block";
     })
 }
 
 // 메인 컨테이너로 마우스 진입시 드롭다운 비활성화
 const container = document.querySelector("#container");
 container.addEventListener("mouseover", e => {
+    // 드롭다운 height를 0으로 지정하여 보이지 않게 변경함
     dropdownWrapper.style.height = "0px";
+    
+    // 드롭다운 하단 이미지를 display: none 옵션을 주어 사라지게함
+    dropdownUnderBG.style.display = "none";
 })
